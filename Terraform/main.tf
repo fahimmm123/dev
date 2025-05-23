3bc84e2681cc0cbb29fb8802c369d76ef947e11d
fahimmm123 <s2109297@students.ncl-coll.ac.uk>
2025-05-23 11:16:08 +0100

    final main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,10 +1,48 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-north-1"
 }
 
+resource "aws_security_group" "php_sg" {
+  name        = "php_sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "php_server" {
-  ami           = "ami-00f34bf9aeacdf007"
+  ami           = "ami-0f9fc25dd2506cf6d"  # Valid Ubuntu AMI for t2.micro in eu-north-1
   instance_type = "t2.micro"
+
+  vpc_security_group_ids = [aws_security_group.php_sg.id]
 
   tags = {
     Name = "PHPApp"
